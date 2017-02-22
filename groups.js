a57cb9599f7e72eed4964649f610b7e888feaa55
Rod Vagg <rod@vagg.org>
2017-02-22 15:00:08 +1100

    account for derpy v6.7.0 release commit summary
    Fixes: https://github.com/rvagg/changelog-maker/issues/42

--- groups.js
+++ groups.js
@@ -14,9 +14,24 @@ function cleanSummary (summary) {
 }
 
 
+/*
+
+to test this, run on the commandline:
+
+for br in v4.x v5.x v6.x v7.x; do git log $br --format='%s' | grep -E '^\d+.*elease$' >> /tmp/release-commits-all.txt; done
+sort /tmp/release-commits-all.txt | uniq > /tmp/release-commits.txt
+rm /tmp/release-commits-all.txt
+
+then in this directory:
+
+node groups.js /tmp/release-commits.txt
+
+doesn't cover false positives though
+*/
+
 function isReleaseCommit (summary) {
   return /^Working on v?\d{1,2}\.\d{1,3}\.\d{1,3}$/.test(summary)
-         || /^\d{4}-\d{2}-\d{2},? Version \d{1,2}\.\d{1,3}\.\d{1,3} (["'][A-Za-z ]+["'] )?\((Current|Stable|LTS|Maintenance)\)/.test(summary)
+         || /^\d{4}-\d{2}-\d{2},? (Node\.js|Version) v?\d{1,2}\.\d{1,3}\.\d{1,3} (["'][A-Za-z ]+["'] )?\((Current|Stable|LTS|Maintenance)\)/.test(summary)
          || /^\d{4}-\d{2}-\d{2},? io.js v\d{1,2}\.\d{1,3}\.\d{1,3} Release/.test(summary)
          || /^\d+\.\d+\.\d+$/.test(summary) // `npm version X` style commit
 }
@@ -25,3 +40,15 @@ function isReleaseCommit (summary) {
 module.exports.toGroups        = toGroups
 module.exports.cleanSummary    = cleanSummary
 module.exports.isReleaseCommit = isReleaseCommit
+
+
+if (require.main == module) {
+  console.log(`Running tests on lines in ${process.argv[2]}...`)
+  var failures = require('fs').readFileSync(process.argv[2], 'utf8').split('\n').filter(Boolean).filter((summary) => {
+    return !isReleaseCommit(summary)
+  })
+  if (!failures.length)
+    return console.log('All good, no failures!')
+  console.log('Failed on the following commit summaries:')
+  console.log(failures.join('\n'))
+}
