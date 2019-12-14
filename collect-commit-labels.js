76f2962e845c0b35a21b2f6eb05b5e9b26b816ff
Jason Etcovitch <jasonetco@github.com>
2019-12-14 18:17:56 -0500

    Use it

--- collect-commit-labels.js
+++ collect-commit-labels.js
@@ -1,8 +1,7 @@
 'use strict'
 
 const ghauth = require('ghauth')
-const ghissues = require('ghissues')
-const async = require('async')
+const { graphql } = require('@octokit/graphql')
 
 const authOptions = {
   configName: 'changelog-maker',
@@ -19,47 +18,42 @@ function collectCommitLabels (list, callback) {
   }
 
   ghauth(authOptions, (err, authData) => {
-    const cache = {}
-
     if (err) {
       return callback(err)
     }
 
-    const q = async.queue((commit, next) => {
-      function onFetch (err, issue) {
-        if (err) {
-          console.error('Error fetching issue #%s: %s', commit.ghIssue, err.message)
-          return next()
-        }
-
-        if (issue.labels) {
-          commit.labels = issue.labels.map((label) => label.name)
-        }
-
-        next()
-      }
-
-      if (commit.ghUser === 'iojs') {
-        commit.ghUser = 'nodejs' // forcibly rewrite as the GH API doesn't do it for us
-      }
-
-      // To prevent multiple simultaneous requests for the same issue
-      // from hitting the network at the same time, immediately assign a Promise
-      // to the cache that all commits with the same ghIssue value will use.
-      const key = `${commit.ghUser}/${commit.ghProject}#${commit.ghIssue}`
-      cache[key] = cache[key] || new Promise((resolve, reject) => {
-        ghissues.get(authData, commit.ghUser, commit.ghProject, commit.ghIssue, (err, issue) => {
-          if (err) {
-            return reject(err)
+    const bits = list.map(item => {
+      return `pr${item.ghIssue}: pullRequest (number: ${item.ghIssue}){
+        title
+        number
+        labels (first: 100) {
+          nodes {
+            name
           }
+        }
+      }`
+    }).join('\n')
 
-          resolve(issue)
-        })
-      })
-      cache[key].then((val) => onFetch(null, val), (err) => onFetch(err))
-    }, 15)
-    q.drain(callback)
-    q.push(sublist)
+    const query = `query ($owner: String!, $name: String!) {
+      repository (owner: $owner, name: $name) {
+        ${bits}
+      }
+    }`
+
+    graphql(query, {
+      headers: {
+        authorization: `token ${authData.token}`
+      },
+      owner: list[0].ghUser,
+      name: list[0].ghProject
+    }).then(res => {
+      for (let i = 0; i < list.length; i++) {
+        const pr = res.repository[`pr${list[i].ghIssue}`]
+        list[i].labels = pr.labels.nodes.map(n => n.name)
+      }
+
+      callback()
+    })
   })
 }
 
