e56d8b537fe40d5380915b788e96ff47356fc914
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-05-04 13:25:50 +0200

    feat: improve support of backtick strings
    Only supports code span delimited with a single backtick char and gives
    up if it finds an edge case.

--- commit-to-output.js
+++ commit-to-output.js
@@ -4,9 +4,30 @@ const chalk = require('chalk')
 const reverts = require('./reverts')
 const groups = require('./groups')
 
+function cleanUnsupportedMarkdown (txt) {
+  // escape _~*\[]<>`
+  return txt.replace(/([_~*\\[\]<>`])/g, '\\$1')
+}
 function cleanMarkdown (txt) {
-  // escape _~*\[]<>
-  return txt.replace(/([_~*\\[\]<>])/g, '\\$1')
+  // Escape backticks for edge case scenarii (no code span support).
+  if (txt.includes('``') || txt.includes('\\`')) {
+    return cleanUnsupportedMarkdown(txt)
+  }
+
+  const backtickSplit = txt.split('`')
+  // If there's an odd number of backticks, give up and escape them all.
+  if (backtickSplit.length % 2 === 0) return cleanUnsupportedMarkdown(txt)
+
+  let cleanMdString = ''
+  for (let i = 0; i < backtickSplit.length; i++) {
+    const isInsideBacktickString = i % 2
+    cleanMdString += isInsideBacktickString
+      // No escaping inside a code span.
+      ? `\`${backtickSplit[i]}\``
+      // otherwise escape _~*\[]<>
+      : backtickSplit[i].replace(/([_~*\\[\]<>])/g, '\\$1')
+  }
+  return cleanMdString
 }
 
 const formatType = {
