00f463c7afb668cc84d4c77888299ca2cb8f6bab
Rod Vagg <rod@vagg.org>
2026-01-22 22:32:42 +1100

    chore: update to ghauth v7, modernise CI

--- auth.js
+++ auth.js
@@ -1,12 +1,10 @@
-import { promisify } from 'util'
 import ghauth from 'ghauth'
 
 const authOptions = {
   configName: 'changelog-maker',
-  scopes: ['repo'],
-  noDeviceFlow: true
+  scopes: ['repo']
 }
 
 export async function auth () {
-  return await promisify(ghauth)(authOptions)
+  return await ghauth(authOptions)
 }
